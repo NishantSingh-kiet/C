c8fdb3dfb4fc78f14cec8c586402c6caefce623c
nishantsingh9084 <55411341+nishantsingh9084@users.noreply.github.com>
2019-09-17 01:34:07 +0530

    Update max_of_four.c

--- max_of_four.c
+++ max_of_four.c
@@ -9,23 +9,23 @@ int max_of_four(int a, int b, int c, int d );
 int main()
  {
     int a, b, c, d;
-    scanf("%d %d %d %d", &a, &b, &c, &d);
+    scanf("%d %d %d %d", &a, &b, &c, &d);      
     int ans = max_of_four(a, b, c, d);
     printf("%d", ans);
     return 0;
 }
 int max_of_four(int a,int b,int c,int d)
 {
-    if(a>b&&a>c&&a>d)   //check condition whether which one is greater
+    if(a>b&&a>c&&a>d)           //check condition for a
     {
         return(a);  //return value of a
 
     }
-    else if(b>c&&b>d)
+    else if(b>c&&b>d)           //check condition for b
     {
         return(b);   //return value of b
     }
-    else if(c>a&& c>b&&c>d)
+    else if(c>a&& c>b&&c>d)     //check condition for c
     {
         return(c);   //return value of c
     }
