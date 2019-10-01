abc57a75fe23c1c515016f254067d62633cb5caf
NishantSingh <55411341+nishantsingh9084@users.noreply.github.com>
2019-10-01 23:57:56 +0530

    Create Star_pattern.c

---
+++ Star_pattern.c
@@ -0,0 +1,34 @@
+//WAP to print star
+#include<conio.h>
+#include<stdio.h>
+void main()
+{
+	int i,j,n;
+	printf("enter the number");
+	scanf("%d",&n);
+	for(i=n;i>=1;i--)
+	{
+		for(j=1;j<=n-i;j++)
+		{
+			printf("*");
+	
+		}
+		
+	printf("\n");
+	}
+	getch();
+}
+
+
+
+
+
+
+
+//output
+
+*
+**
+***
+****
+*****
