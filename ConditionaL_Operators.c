89f09baa727f85db5a4e25aec1695ee7b0391cd6
NishantSingh <55411341+nishantsingh9084@users.noreply.github.com>
2019-10-10 16:04:16 +0530

    Create ConditionaL_Operators.c

---
+++ ConditionaL_Operators.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+int n;
+scanf("%d",&n);
+if(n%2!=0)
+  {
+  printf("Weird");
+  }
+else
+  {
+    if(n>=2&&n<=5)
+    {
+    printf("Not Weird");
+    }
+    else if(n>=6&&n<=20)
+    {
+    printf("Weird");
+    }
+    else
+    {
+    printf("Not Weird");
+    }
+  }
+return 0;
+}
